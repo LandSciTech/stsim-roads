c8827caab0cf3e8e90925992832361c6015fa888
alex <alexe_webmail@yahoo.com>
2019-05-08 12:24:54 -0700

    Map and runtime updates to config and script.

--- src/transformer.R
+++ src/transformer.R
@@ -1,2 +1,46 @@
+library(methods)
 library(rsyncrosim)
+
+GetDataSheetExpectData <- function(name, ssimObj) {
+  ds = datasheet(ssimObj, name)
+  if (nrow(ds) == 0) { stop(paste0("No data for: ", name)) }
+  return(ds)
+}
+
+GetSingleValueExpectData <- function(df, name) {
+  v = df[, name]
+  if (is.na(v)) { stop(paste0("Missing data for: ", name)) }
+  return(v)
+}
+
+#Environment
+e = ssimEnvironment()
+GLOBAL_Session = session("c:/gitprojects/ssimbin")
+GLOBAL_Library = ssimLibrary(name = e$LibraryFilePath, session = GLOBAL_Session)
+GLOBAL_Project = project(GLOBAL_Library, project = as.integer(e$ProjectId))
+GLOBAL_Scenario = scenario(GLOBAL_Library, scenario = as.integer(e$ScenarioId))
+GLOBAL_RunControl = GetDataSheetExpectData("STSim_RunControl", GLOBAL_Scenario)
+GLOBAL_MaxIteration = GetSingleValueExpectData(GLOBAL_RunControl, "MaximumIteration")
+GLOBAL_MinIteration = GetSingleValueExpectData(GLOBAL_RunControl, "MinimumIteration")
+GLOBAL_MinTimestep = GetSingleValueExpectData(GLOBAL_RunControl, "MinimumTimestep")
+GLOBAL_MaxTimestep = GetSingleValueExpectData(GLOBAL_RunControl, "MaximumTimestep")
+GLOBAL_TotalIterations = (GLOBAL_MaxIteration - GLOBAL_MinIteration + 1)
+GLOBAL_TotalTimesteps = (GLOBAL_MaxTimestep - GLOBAL_MinTimestep + 1)
+
+#Simulation
+envBeginSimulation(GLOBAL_TotalIterations * GLOBAL_TotalTimesteps)
+
+for (iteration in GLOBAL_MinIteration:GLOBAL_MaxIteration) {
+
+  for (timestep in GLOBAL_MinTimestep:GLOBAL_MaxTimestep) {
+
+    envReportProgress(iteration, timestep)
+
+    
+
+    envStepSimulation()
+  }
+}
+
+envEndSimulation()
 
